13b1f6fa0f19c54115298ce17b2a47edc027f6bd
Philip Colmer <philip.colmer@linaro.org>
2023-07-07 08:08:20 +0100

    Loosen versionFrom requirement

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,7 +15,7 @@ const projects = defineCollection({
     icon: z.string(),
     categories: z.array(reference("categories")),
     link: z.string().url(),
-    versionFrom: z.string(),
+    versionFrom: z.any(), // temporary solution to accepting something like 3.11 as a string
     support: z.enum(["native", "emulation", "no", "unknown"]),
     notes: z.string(),
   }),
