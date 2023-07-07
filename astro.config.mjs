a1a68ac35fb5c2f1e42361d0eb2220ef582c9516
Philip Colmer <philip.colmer@linaro.org>
2023-07-07 09:42:21 +0100

    Remove build format

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  build: {
-    format: "folder",
-  },
   output: "static",
   integrations: [mdx(), sitemap(), tailwind()],
 });
