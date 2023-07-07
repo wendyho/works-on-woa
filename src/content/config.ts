67d737e8db5168d40f0730a977e1014dd793c172
Philip Colmer <philip.colmer@linaro.org>
2023-07-07 08:01:25 +0100

    Add versionFrom

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,6 +15,7 @@ const projects = defineCollection({
     icon: z.string(),
     categories: z.array(reference("categories")),
     link: z.string().url(),
+    versionFrom: z.string(),
     support: z.enum(["native", "emulation", "no", "unknown"]),
     notes: z.string(),
   }),
