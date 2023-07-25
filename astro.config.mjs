ff3f52482029adc9add64b0fe93d9a58effebfea
Louis Morgan <louis.morgan@linaro.org>
2023-07-25 14:28:45 +0100

    add styles to homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   output: "static",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), pagefind()],
   cacheDir: "./cache",
   compressHTML: true,
 });
