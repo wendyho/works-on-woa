d5a23eae5b5a75d0f7f49d8e90bad6eb6a76b7e3
Louis <louis.morgan@linaro.org>
2024-03-03 12:39:38 +0000

    create custom pagefind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import aws from "astro-sst";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import pagefind from "astro-pagefind"
+import pagefind from "./integrations/pagefind"
 import auth from "auth-astro";
 
 // https://astro.build/config
@@ -12,18 +12,19 @@ export default defineConfig({
   adapter: aws(),
   site: "https://www.worksonwoa.com",
   integrations: [
-    // sitemap(), 
-    // pagefind(),
- tailwind({
-    applyBaseStyles: false
-  }), solidJs(), auth({
-    injectEndpoints: false,
-  })],
+    sitemap(),
+    pagefind(),
+    tailwind({
+        applyBaseStyles: false
+      }), solidJs(), auth({
+        injectEndpoints: false,
+      })],
   cacheDir: "./cache",
   compressHTML: true,
   build: {
     rollupOptions: {
       external: ["/_pagefind/pagefind.js"]
+      external: ["/pagefind/pagefind.js"]
     },
     
   }
---
+++ integrations/pagefind.ts
@@ -0,0 +1,52 @@
+import type { AstroIntegration } from "astro";
+import { fileURLToPath } from "node:url";
+import { execSync } from "child_process";
+import sirv from "sirv";
+
+export default function pagefind(): AstroIntegration {
+    let outDir: string;
+    return {
+        name: "pagefind",
+        hooks: {
+            "astro:config:setup": ({ config, logger }) => {
+                outDir = "./dist_pagefind/dist/client"
+            },
+            "astro:server:setup": ({ server, logger }) => {
+                if (!outDir) {
+                    logger.warn(
+                        "astro-pagefind couldn't reliably determine the output directory. Search assets will not be served.",
+                    );
+                    return;
+                }
+                logger.warn(outDir)
+                const serve = sirv(outDir, {
+                    dev: true,
+                    etag: true,
+                });
+                server.middlewares.use((req, res, next) => {
+                    if (req.url?.startsWith("/pagefind/")) {
+                        serve(req, res, next);
+
+                    } else {
+                        next();
+                    }
+                });
+            },
+            "astro:build:done": ({ logger }) => {
+                logger.warn(outDir)
+
+                if (!outDir) {
+                    logger.warn(
+                        "astro-pagefind couldn't reliably determine the output directory. Search index will not be built.",
+                    );
+                    return;
+                }
+
+                const cmd = `npx pagefind --site "${outDir}"`;
+                execSync(cmd, {
+                    stdio: [process.stdin, process.stdout, process.stderr],
+                });
+            },
+        },
+    };
+}
